b8b8e79b3a5e4b1d404d31eb492e4be844fd6698
andrewhannebrink <andrewhannebrink@gmail.com>
2016-01-13 21:35:01 -0800

    router is a bit more dynamic - going to push to web now

--- static/js/router.js
+++ static/js/router.js
@@ -2,16 +2,24 @@
   var R = Backbone.Router.extend({
   
     routes: {
-      'm-:lib': 'mosaic'
+      'm-:lib/:sz(/:bg)': 'mosaic'
     }
   
   });
   
   var r = new R;
   
-  r.on('route:mosaic', function(lib) {
-    console.log('reached mosaic route');
+  r.on('route:mosaic', function(lib, sz, bg) {
+    console.log('hit mosaic route');
     pmpm.cmp.attributes.lib = lib;
+    pmpm.cmp.tileX = sz;
+    pmpm.cmp.tileY = sz;
+    if (bg !== null) {
+      pmpm.cmp.opt.bg = bg;
+    } else {
+      pmpm.cmp.opt.bg = undefined;
+    }
+    pmpm.makeMosaic(pmpm.cmp);
   });
   
   Backbone.history.start();
